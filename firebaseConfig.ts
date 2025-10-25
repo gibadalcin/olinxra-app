1adcdf37d906b213ac26ac06770092e964d6425e
Gilberto Dalcin <dalcin.tools@outlook.com>
2025-10-25 20:36:02 -0300

    fix: variaveis

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import type { Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || "AIzaSyCXQ4q3E_GaMmpqnAyFy_qHZKoB9wZhKW8",
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || "olinxra-app-1a027.firebaseapp.com",
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || "olinxra-app-1a027",
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || "olinxra-app-1a027.firebasestorage.app",
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "517113888972",
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || "1:517113888972:web:4e7b7eb8d5984f9b3dc269"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
