160d10bafbc180dafa72e019e9b97d59faa9ccc4
Gilberto Dalcin <dalcin.tools@outlook.com>
2025-11-06 18:21:29 -0300

    fix: substitui função preterida e imports não utilizados

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -1,7 +1,12 @@
-import * as React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 
+import MyComponent from '../MyComponent';
 import { MonoText } from '../StyledText';
+
+test('renderiza corretamente', () => {
+  const { getByText } = render(<MyComponent />);
+  expect(getByText('Texto esperado')).toBeTruthy();
+});
 
 it(`renders correctly`, () => {
   const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
